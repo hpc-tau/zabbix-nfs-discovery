d531d62234ef5458f8db4cd1beac57629bdea467
hpc-tau <118801434+hpc-tau@users.noreply.github.com>
2022-12-25 16:25:32 +0200

    Update nfs_discovery.py

--- nfs_discovery.py
+++ nfs_discovery.py
@@ -3,12 +3,17 @@
 import json
 
 DISCOVERY_LIST = []
-with open('/etc/fstab', 'rt') as f:
+with open('fstab', 'rt') as f:
     data = f.readlines()
 for line in data:
-    if 'nfs' in line:
+    if 'nfs' in line and not line.startswith('#'):
+    # if 'nfs' in line:
         # print(line.split()[1])
         DISCOVERY_LIST.append({"{#NFSNAME}": line.split()[1]})
 
 JSON = json.dumps(DISCOVERY_LIST)
-print(JSON)
+# print(JSON)
+
+for i in json.loads(JSON):
+    print(i)
+print(len(DISCOVERY_LIST))
